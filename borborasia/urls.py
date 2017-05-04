fc36b25cace2aabd9503967520e06384005d693c
katalyzator <web.coder96@gmail.com>
2017-05-04 15:26:23 +0600

    something changes

--- borborasia/urls.py
+++ borborasia/urls.py
@@ -28,7 +28,9 @@ urlpatterns = [
     url(r'^todo/', 'main.views.todo_view', name='todo'),
     url(r'^reviews/', 'main.views.review_view', name='review'),
     url(r'^regions/', 'main.views.region_view', name='region'),
+    url(r'^cart/', 'main.views.get_cart', name='cart'),
     url(r'^tour/(?P<id>\d+)/$', 'main.views.single_tour', name='single_tour'),
+    url(r'^add_to_cart/(?P<id>\d+)/$', 'main.views.add_to_cart', name='add_to_card'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

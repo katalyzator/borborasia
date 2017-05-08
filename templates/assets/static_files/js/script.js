5e7981dbd5d2175459ab6ea80528eebd42f99075
katalyzator <web.coder96@gmail.com>
2017-05-08 14:01:41 +0600

    added new changes

--- templates/assets/static_files/js/script.js
+++ templates/assets/static_files/js/script.js
@@ -1,4 +1,39 @@
+function OpenPopup(){
+    var sblock = $('.search_drop');
+    sblock.toggle(300);
+}
+
+$(document).ready(function () {
+    $('.extra_menu > li:first-child').click(OpenPopup);
+});
+
 (function () {
+
+    //// get the name of uploaded file
+    //$('input[type="file"]').change(function(){
+    //    var value = $("input[type='file']").val();
+    //    $('.js-value').text(value);
+    //});
+
+    $('#custom-file-upload').change(function(e) {
+        var filename = e.target.files[0].name;
+        console.log(filename);
+        $('.filupp-file-name').html(filename);
+    });
+
+    $('.menu > li > a').on('click', function () {
+        //event.preventDefault();
+        $('.menu > li a').removeClass('link_active');
+        $(this).addClass('link_active');
+    });
+
+    $('.language > li:first-child a').addClass('l_active');
+    $('.language > li > a').on('click', function (event) {
+        event.preventDefault();
+        $('.language > li a').removeClass('l_active');
+        $(this).addClass('l_active');
+    });
+
     $('.slider').slick({
         cssEase: 'ease-in',
         autoplay: true,
@@ -7,9 +42,6 @@
         dots: true
     });
 
-    // get the name of uploaded file
-    $('input[type="file"]').change(function () {
-        var value = $("input[type='file']").val();
-        $('.js-value').text(value);
-    });
 })();
+
+
